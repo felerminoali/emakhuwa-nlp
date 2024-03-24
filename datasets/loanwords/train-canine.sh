f74efd5768bf5c98133c8e9f0c28e1120f5160fe
felerminoali <felerminoali@gmail.com>
2024-03-25 01:45:11 +0200

    version 1.0

--- datasets/loanwords/train-canine.sh
+++ datasets/loanwords/train-canine.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "Running Python code CANINE ONE"
+model_file="checkpoints/model_one.pt"
 python train-canine-one.py -ms "$model_file" -psp predictions/canine/ -dp data/canine/ -mn google/canine-c -tn google/canine-c
 
 echo "Running Python code CANINE CONCAT"
-python train-canine-concat.py -ms "$model_file" -psp predictions/canine/ -dp data/canine/ -mn google/canine-c -tn google/canine-c
+model_file="checkpoints/model_concat.pt"
+python train-canine-concat.py -ms "$model_file" -psp predictions/canine/ -dp data/${pair}/canine/ -mn google/canine-c -tn google/canine-c
